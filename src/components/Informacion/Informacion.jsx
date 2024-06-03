9b09db7c3e23e384f0aab6922b2a8b30d199ba32
Santiago Jaramillo <santiagojaramillo2611@gmail.com>
2024-06-03 11:06:47 -0500

    Start project

---
+++ src/components/Informacion/Informacion.jsx
@@ -0,0 +1,11 @@
+import './Informacion.css'
+
+export const Informacion = () => {
+  return (
+    <>
+    <div className="info">Informacion
+    <div><h1>hola</h1></div>
+    </div>
+    </>
+  )
+}
